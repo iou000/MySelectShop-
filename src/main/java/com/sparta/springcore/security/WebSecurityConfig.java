29d12ae69a8582f085baebafe71fa9aec027647b
iou000 <iou000@naver.com>
2021-11-07 19:00:20 +0900

    카카오 로그인 구현

--- src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.sparta.springcore.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +19,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder encodePassword() {
         //BCryptPasswordEncoder 객체를 만들어서 IoC컨테이너에 넣어줌.
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 
