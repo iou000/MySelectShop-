f592ef538c8e75a8074a88e28497dad694a54f17
iou000 <iou000@naver.com>
2021-11-28 00:56:34 +0900

    폴더 생성 기능+상품에 폴더 추가 기능

--- src/main/java/com/sparta/springcore/controller/HomeController.java
+++ src/main/java/com/sparta/springcore/controller/HomeController.java
@@ -1,17 +1,32 @@
 package com.sparta.springcore.controller;
 
+import com.sparta.springcore.model.Folder;
 import com.sparta.springcore.security.UserDetailsImpl;
+import com.sparta.springcore.service.FolderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    private final FolderService folderService;
+
+    @Autowired
+    public HomeController(FolderService folderService) {
+        // 멤버 변수 생성
+        this.folderService = folderService;
+    }
+
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         // @AuthenticationPrincipal는 스프링 시큐리티에서 로그인된 사용자의 정보를 받아옴
+        List<Folder> folders = folderService.getFolders(userDetails.getUser());
+        model.addAttribute("folders", folders);
         model.addAttribute("username", userDetails.getUsername());
         return "index";
     }
